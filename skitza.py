a3596eaf8ec224aadd2ed0ff0a6fc65dcab866a2
Vladimir Zeifman <edoomik@gmail.com>
2016-12-02 13:28:50 +0200

    added `--config` option to specify path to config file
    - if `--config` is not specified, skitza will try to load `skitza.json` in current directory
    - if `--config` is not specified and `skitza.json` missing, message with usage explanation will be presented to the user

--- skitza.py
+++ skitza.py
@@ -2,23 +2,23 @@ import os
 import json
 import click
 import jinja2
+import sys
 
-
-f = open('skitza.json', 'r')
-config = json.load(f)
-f.close()
 
 SKITZA_CONSTANTS = {
     'cwd': os.getcwd(),
     'env': os.environ
 }
 
+SKITZA_DEFAULT_CONFIG = 'skitza.json'
+
+
 @click.group()
 def cli():
     pass
 
 
-def command(com):
+def command(config, com):
     def inner(*args, **kwargs):
         # kwargs contain arguments that were passed to the command
         kwargs['constants'] = config['constants']
@@ -33,10 +33,10 @@ def command(com):
     return inner
 
 
-def register_commands():
+def register_commands(config):
     commands = []
     for idx, com in enumerate(config['commands']):
-        commands.append(command(com)) # commands stores `command` functions
+        commands.append(command(config, com)) # commands stores `command` functions
         for arg in com['arguments']:
             commands[idx] = click.option('--{option}'.format(option=arg))(
                 commands[idx]
@@ -71,6 +71,53 @@ def create_directory(path, options):
     if not os.path.exists(rendered_path):
         os.makedirs(rendered_path)
 
+
+def get_option_from_argv(argv, name):
+    for idx, arg in enumerate(argv):
+        if arg.startswith(name):
+            return argv[idx]
+
+
+def get_value_from_option(opt):
+    splitted = opt.split('=')
+
+    if len(splitted) < 2:
+        return splitted[0], None
+    else:
+        return splitted[0], splitted[1]
+
 if __name__ == '__main__':
-    register_commands()
+    config_opt = get_option_from_argv(sys.argv, '--config=')
+
+    if config_opt:
+        # remove config path from sys.argv so click module won't process it.
+        sys.argv.remove(config_opt)
+        (option_name, option_value) = get_value_from_option(config_opt)
+        config_path = option_value
+    else:
+        # fall back to default config name that may reside in current directory
+        config_path = SKITZA_DEFAULT_CONFIG
+
+    try:
+        f = open(config_path, 'r')
+    except IOError as error:
+        if config_path == SKITZA_DEFAULT_CONFIG:
+            sys.exit('Unable to find local skitza.json and `--config` was not specified, aborting.\n\nUsage: skitza.py '
+                     '[OPTIONS]\n\nOptions:\n  --config   Path to skitza *.json config file')
+        else:
+            sys.exit('ERROR: Could not read `{path}` config file. Reason: {reason}'.format(
+                path=error.filename,
+                reason=error.strerror
+            ))
+    try:
+        config_json = json.load(f)
+    except ValueError as error:
+        sys.exit('ERROR: Could not parse `{path}` config file. Reason: {reason}'.format(
+            path=config_path,
+            reason=error.message
+        ))
+    finally:
+        f.close()
+
+    register_commands(config_json)
     cli()
