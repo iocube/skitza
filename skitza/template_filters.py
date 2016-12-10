576f888665d9af22b37925a1cb7ab4c8ed13b225
Vladimir Zeifman <edoomik@gmail.com>
2016-12-10 15:24:14 +0200

    fixed filters.py throwing an error because input was None
    - updated link about custom filters

--- skitza/template_filters.py
+++ skitza/template_filters.py
@@ -1,6 +1,17 @@
-# Read: http://jinja.pocoo.org/docs/dev/templates/#builtin-filters
+# Read: http://jinja.pocoo.org/docs/dev/api/#custom-filters
+
+
+def first_upper(s):
+    if s:
+        return s[0].upper() + s[1:]
+
+
+def first_lower(s):
+    if s:
+        return s[0].lower() + s[1:]
+
 
 filters = {
-    'first_upper': lambda s: s[0].upper() + s[1:],
-    'first_lower': lambda s: s[0].lower() + s[1:]
+    'first_upper': first_upper,
+    'first_lower': first_lower
 }
